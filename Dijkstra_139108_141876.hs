57e0dccabf73f9787fe6c1779da3b2160124057f
CoolTarka <mb_goldmann@hotmail.com>
2017-05-28 02:14:05 -0300

    definição do record e main

--- Dijkstra_139108_141876.hs
+++ Dijkstra_139108_141876.hs
@@ -3,6 +3,7 @@
 -- Rudolfo Goldmann Neto 139108
 
 import System.IO
+import Data.List
 
 data No = No {
   nome::String,
@@ -11,9 +12,15 @@ data No = No {
 } deriving(Show)
 
 main = do
-  tst <- openFile "testes/teste0.in" ReadMode
-  entrada <- hGetContents tst
-  putStrLn("teste:" ++ entrada)
+  file <- openFile "testes/teste0.in" ReadMode
+  -- entrada <- hGetContents file
+  entrada <- hGetLine file
+  let
+    final = reads $ entrada
+  putStrLn final
 
 -- retorna lista de valores de vizinhos e distancias
 getViz (No _ vizinhos _) = vizinhos
+
+-- tst [] = []
+tst ('a  b d':xs) = read x
