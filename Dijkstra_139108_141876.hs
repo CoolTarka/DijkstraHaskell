f1a84b32d53b9ff94e750fa9b49c198281092f46
CoolTarka <mb_goldmann@hotmail.com>
2017-05-29 15:05:48 -0300

    Codigo ta separando a entrada em lista de no com seus vizinhos

--- Dijkstra_139108_141876.hs
+++ Dijkstra_139108_141876.hs
@@ -20,9 +20,13 @@ main = do
     pre =  getlist $ lines entrada
     -- path tem o caminho a ser procurado pelo codigo
     path = concat [z | z <- pre, length z == 1]
-    -- graph tem todos as especificaçoes de no
+    -- graph tem todos as especificaçoes de no e seus vizinhos
     graph = [z | z <- pre, length z == 3]
-  print graph
+    -- nodes tem todos os nos do grafo
+    nodes = get_nodes (map head graph) 0
+    tst = pre_graph nodes graph
+    tst2 = head tst
+  print tst2
 
 -- retorna lista de valores de vizinhos e distancias
 getViz (No _ vizinhos _) = vizinhos
@@ -33,13 +37,32 @@ getlist [] = []
 getlist (x:xs) =
   words x : getlist xs
 
+-- tentantiva de organizar valores pra por no grafo
+pre_graph [] _ = []
+pre_graph (x:xs) lista =
+  [[(x,0)]:create_no x lista:[]] ++ pre_graph xs lista
+
 -- retorna todos os vizinhos em uma lista de tuplas (vizinho,distancia) pra um no só
 create_no node [] = []
 create_no node ((no:viz:val:[]):xs) =
-  if node == no then (viz, read val :: Integer): create_no node xs
-  else []
+  if node == no then (viz, read val :: Float): create_no node xs
+  else create_no node xs
 
+-- pega todos elementos iguais consecutivos e mantem apenas um, tem um exmeplo ai em baixo
+get_nodes [] _ = []
+get_nodes (x:xs) val
+   | null y = []
+   | x == y && val == 0 = y : get_nodes xs 1
+   | x /= y = y : get_nodes xs 1
+   | otherwise  = get_nodes xs 1
+   where
+     y = if null xs then [] else head xs
+-- EXEMPLO
+-- get_nodes ["a","a","a","a","b","b","b","c","d","d","d","d","d","e"] 0
+-- > ["a","b","c","d","e"]
+
+-- #########################################################################
 -- funcao de um broder mutante da internet, nao funciona aqui
-myGroup :: (Eq a, Ord a) => [(a, b)] -> [(a, [b])]
-myGroup = map (\l -> (fst . head $ l, map snd l)) . groupBy ((==) `on` fst)
-          . sortBy (comparing fst)
+-- myGroup :: (Eq a, Ord a) => [(a, b)] -> [(a, [b])]
+-- myGroup = map (\l -> (fst . head $ l, map snd l)) . groupBy ((==) `on` fst)
+--           . sortBy (comparing fst)
